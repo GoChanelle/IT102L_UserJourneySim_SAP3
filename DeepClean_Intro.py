162bac2631a43972dd83da415d58d67f9dd09a8e
GoChanelle <gochanellemei@gmail.com>
2026-09-12 14:55:25 +0000

    Fix indentation

--- DeepClean_Intro.py
+++ DeepClean_Intro.py
@@ -14,22 +14,22 @@ def show_introduction():
                " Goodluck, :red[Don't Drown].")
 
     
-    def check_mirror():
-        if "show_mirror" not in st.session_state:
+def check_mirror():
+    if "show_mirror" not in st.session_state:
             st.session_state.show_mirror = False
 
-        if st.button("Check Mirror", width="stretch"):
+    if st.button("Check Mirror", width="stretch"):
             st.session_state.show_mirror = not st.session_state.show_mirror
 
-        if st.session_state.show_mirror:
-            col4 = st.columns(1, border=True)[0]
-            col4.write("You check your gear in the mirror...")
+    if st.session_state.show_mirror:
+        col4 = st.columns(1, border=True)[0]
+        col4.write("You check your gear in the mirror...")
 
-            gear = st.session_state.gear
-            for item in gear.gear_inventory():
-                status = gear.gear_status[item]
-                color = "red" if status == "Broken" else "green"
-                col4.write(f"{item}: :{color}[{status}]")
+        gear = st.session_state.gear
+        for item in gear.gear_inventory():
+            status = gear.gear_status[item]
+            color = "red" if status == "Broken" else "green"
+            col4.write(f"{item}: :{color}[{status}]")
 
 def check_outside():
     if "show_outside" not in st.session_state:
