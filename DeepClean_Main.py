0037326760814048f03d6d3ff99ad6e49339b580
GoChanelle <gochanellemei@gmail.com>
2026-09-12 13:44:16 +0000

    Fix Messages

--- DeepClean_Main.py
+++ DeepClean_Main.py
@@ -35,15 +35,15 @@ if menu == "Introduction":
 
     col1 = st.columns(1, border=True)[0]
     col1.write("It's the year 20xx. Less of the world is becoming inhabitable."
-               "The air around the coastline is thick and the waters are dangerous.")
+               " The air around the coastline is thick and the waters are dangerous.")
 
     col2 = st.columns(1, border=True)[0]
     col2.write("The coastline is prohibited to anyone who isn't a Diver."
-               "Unfortunately, you are a :red[Diver].")
+               " Unfortunately, you are a :red[Diver].")
 
     col3 = st.columns(1, border=True)[0]
     col3.write("Your job is to maintain the Trash Beasts for a hopefully better future."
-                   "Goodluck, :red[Don't Drown].")
+                   " Goodluck, :red[Don't Drown].")
 
 elif menu == "Load Game":
     st.write("This is a simulated experience. You cannot load any previous attempts ):")
