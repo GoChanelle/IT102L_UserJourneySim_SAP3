f518d15eb3a5faf711e5a1abb1908160ce78e113
GoChanelle <gochanellemei@gmail.com>
2026-09-12 11:01:55 +0000

    Initial Commit

---
+++ DeepClean_CreatureInfo.py
@@ -0,0 +1,5 @@
+class Creature:
+    def __init__(self, name, effect, score):
+        self.name = name
+        self.effect = effect
+        self.score = score
---
+++ DeepClean_Main.py
@@ -0,0 +1,6 @@
+from DeepClean_CreatureInfo import Creature
+
+creature1 = Creature("Bag", "Oxygen Depletion", 90)
+creature2 = Creature("Plastic Ring ", "Oxygen Depletion", 60)
+
+print(creature1)
