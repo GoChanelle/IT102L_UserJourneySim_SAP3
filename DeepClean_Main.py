feeae3c0a04dfb7bcf4d4e2cc605e8d274ee9c79
GoChanelle <gochanellemei@gmail.com>
2026-09-12 12:45:50 +0000

    v

--- DeepClean_Main.py
+++ DeepClean_Main.py
@@ -5,24 +5,11 @@ from DeepClean_CreatureInfo import Creature
 # PAGE CONFIG -----------------------------------------------------------
 st.set_page_config(
   page_title="DeepClean",
-  page_icon="🏦",
+  page_icon="🌊",
   layout="wide"
 )
 
 # PAGE TITLE ------------------------------------------------------------
-background_image = """
-<style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://static.wikia.nocookie.net/limbuscompany/images/e/e2/Story_boat_cloudy2.png/revision/latest/scale-to-width-down/985?cb=20231120093636");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-attachment: local;
-}
-</style>
-"""
-
-st.html(background_image)
 
 st.title("Deep Sea Cleaning")
 st.subheader("The sea is in shambles.")
