160164f6d22047dec6e7a91c24e5e7ea3430334c
GoChanelle <gochanellemei@gmail.com>
2026-09-12 13:43:14 +0000

    Intro Messages

--- DeepClean_Main.py
+++ DeepClean_Main.py
@@ -16,6 +16,8 @@ st.subheader("The sea is in shambles.")
 st.caption("Rough simulation for IT102L Exam")
 st.divider()
 
+# MENU OPTIONS -----------------------------------------------------------
+
 options = ["Introduction", "Check Gear", "Dive", "Creature Index"]
 
 if "menu" not in st.session_state:
@@ -27,16 +29,21 @@ for option in options:
         st.session_state.menu = option
 menu = st.session_state.menu
 
+# STORY INTRODUCTION -----------------------------------------------------
 if menu == "Introduction":
     st.caption("This is where the game would start")
 
     col1 = st.columns(1, border=True)[0]
-    col1.write("It's the year 20xx. Less of the world is becoming inhabitable.\n"
+    col1.write("It's the year 20xx. Less of the world is becoming inhabitable."
                "The air around the coastline is thick and the waters are dangerous.")
 
     col2 = st.columns(1, border=True)[0]
-    col2.write("The coastline is prohibited to anyone who isn't a Diver.\n"
+    col2.write("The coastline is prohibited to anyone who isn't a Diver."
                "Unfortunately, you are a :red[Diver].")
+
+    col3 = st.columns(1, border=True)[0]
+    col3.write("Your job is to maintain the Trash Beasts for a hopefully better future."
+                   "Goodluck, :red[Don't Drown].")
 
 elif menu == "Load Game":
     st.write("This is a simulated experience. You cannot load any previous attempts ):")
