f008523e8ba2777be43ae30117fe0e33cbf5ebce
GoChanelle <gochanellemei@gmail.com>
2026-09-13 23:43:04 +0800

    Rename Module

--- DeepClean_Main.py
+++ DeepClean_Main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from DeepClean_Intro import show_introduction, check_mirror, check_outside
 from DeepClean_Gear import Gear
-from DeepClean_Shop import repair_station, gear_shop
+from DeepClean_CheckGear import repair_station, gear_shop
 
 # PAGE CONFIG -----------------------------------------------------------
 st.set_page_config(
